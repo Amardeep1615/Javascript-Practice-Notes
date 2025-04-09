394b2e25304a948a6cf35f25bdb02cce4bde8983
Amardeep1615 <bolagani.amardeep1627@gmail.com>
2025-04-09 15:55:21 +0530

    DAY 2 -Variables,Scope of variables

---
+++ Notesjs/theory.js
@@ -0,0 +1,99 @@
+
+//--------------------------------------------------Day 1-------------------------------------------------------------------------------->
+//--<h1>JavaScript is a programming language used to create dynamic content for websites.</h1> 
+   //<h2>It is a lightweight, cross platform,and single threaded programming language.</h2>
+   //<h2>It is an interpreted language that executes code line by line providing more flexibility.</h2>-->
+    
+   //Key features of JavaScript
+    //1. Dynamic typing
+    //2. Event-Driven
+    //3. Ligtweighted and interpreted
+    //4. Object -oriented
+    //5. Cross-platform
+    //6. Versatile used in both FE and BE
+    //7. Asynchrnous: Handles the multiple tasks at a time by using async,await
+    //8. Security and Validation
+//...........................................................DAY 2....................................................................................................................................
+//................................................Variables and its types with examples...........................................................................................................
+
+//1.Variable:Variables are Containers for Storing Data
+//2.JavaScript Variables can be declared in 4 ways:
+//Automatically
+//1.var
+//2.let
+//3.const
+
+
+//JavaScript Var
+//The var keyword was introduced in ES3 (2009)
+
+//Variables declared with var have Function Scope
+
+//Variables declared with var can be Redeclared,Reassigned in the same scope
+
+
+//JavaScript Let
+//The let keyword was introduced in ES6 (2015)
+
+//Variables declared with let have Block Scope
+
+//Variables declared with let must be Declared before use
+
+//Variables declared with let cannot be Redeclared in the same scope
+
+//JavaScript Const
+//The const keyword was introduced in ES6 (2015)
+
+//Variables declared with const have Block Scope
+
+//Variables declared with let must be Declared before use
+
+//Variables declared with const cannot be Redeclared,Reassigned in the same scope
+
+//................................................Scope of Variables ...........................................................................................................
+// They are 3 types of scope in JavaScript
+//1. Global Scope : Variables declared outside any function/block. Accessible everywhere.
+//2. Function Scope,Local Scope:Variables declared inside a function. Accessible only within that function.
+//3. Block Scope :Variables declared inside a block of code like if,else,switch,for,while,try..
+
+//***var – Function Scoped***
+//var is function-scoped and hoisted (moved to the top of its scope). It doesn't respect block scope (like if, for blocks).
+
+//***let – Block Scoped***
+//let is block-scoped and not hoisted in a usable way. It’s the preferred way to declare mutable variables.
+
+//***const – Block Scoped, Constant***
+//const is block-scoped like let, but you cannot reassign a new value to it after initialization.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+   
