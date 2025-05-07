7ee74baac7fe3908eec0579437861d093d9c0ae6
Amardeep1615 <bolagani.amardeep1627@gmail.com>
2025-05-07 10:39:55 +0530

    DOM-Task1

---
+++ Notesjs/DOM/domtask1.js
@@ -0,0 +1,4 @@
+function changeParagraphText() {
+    let changetext = document.getElementById("para1");
+    changetext.textContent = "The DOM represents the structure of an HTML or XML document as a tree of objects. It allows JavaScript (or other languages) to access, modify, and interact with elements on a web page.";
+}
